dce2f4d938a79bb64bb44166a0ab6aacdc19b43b
mkozakk <mich.kozak@proton.me>
2026-01-15 18:01:43 +0100

    init: create project boilerplate

---
+++ backend/enrolment-service/src/main/java/dev/pulsermm/enrolment/EnrolmentApplication.java
@@ -0,0 +1,11 @@
+package dev.pulsermm.enrolment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolmentApplication.class, args);
+    }
+}
---
+++ backend/metric-service/src/main/java/dev/pulsermm/metric/MetricApplication.java
@@ -0,0 +1,11 @@
+package dev.pulsermm.metric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetricApplication.class, args);
+    }
+}
