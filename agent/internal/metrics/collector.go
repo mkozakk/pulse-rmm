f22af7c2bc1b2898bd546a3746dc2a836b44c146
mkozakk <mich.kozak@proton.me>
2026-01-21 22:12:24 +0100

    feat(agent): heartbeat, metrics ingestion, daemon mode

---
+++ agent/internal/metrics/collector.go
@@ -0,0 +1,43 @@
+package metrics
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+type Sample struct {
+	Type        string
+	Value       float64
+	CollectedAt time.Time
+}
+
+func Collect() ([]Sample, error) {
+	now := time.Now()
+	var samples []Sample
+
+	cpuPct, err := cpu.Percent(0, false)
+	if err != nil {
+		return nil, fmt.Errorf("collecting cpu: %w", err)
+	}
+	if len(cpuPct) > 0 {
+		samples = append(samples, Sample{Type: "cpu", Value: cpuPct[0], CollectedAt: now})
+	}
+
+	vmStat, err := mem.VirtualMemory()
+	if err != nil {
+		return nil, fmt.Errorf("collecting ram: %w", err)
+	}
+	samples = append(samples, Sample{Type: "ram", Value: vmStat.UsedPercent, CollectedAt: now})
+
+	diskStat, err := disk.Usage("/")
+	if err != nil {
+		return nil, fmt.Errorf("collecting disk: %w", err)
+	}
+	samples = append(samples, Sample{Type: "disk", Value: diskStat.UsedPercent, CollectedAt: now})
+
+	return samples, nil
+}
