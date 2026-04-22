999856123993894f38adee86f1228bc9d1db5421
mkozakk <michalkozakk0@gmail.com>
2026-04-22 08:38:11 +0200

    feat(gateway): route /api/auth to identity-service

---
+++ backend/api-gateway/src/test/java/dev/pulsermm/gateway/GatewayRoutingTest.java
@@ -0,0 +1,12 @@
+package dev.pulsermm.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayRoutingTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
